2dc049d6ab64d1d24e5bebde8db26c0426d2b052
silentmonk1962 <abhishek.singh@iiml.org>
2019-04-17 16:16:13 +0530

    Making app ready for heroku deployment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const PORT= 3000;
+const PORT= process.env.PORT ||3000;
 const path =require('path');
 const hbs=require('hbs');
 const geocode=require('./utils/geocode');
@@ -89,4 +89,4 @@ app.get('*',(req,res)=>{
     });
 });
 
-app.listen(PORT,()=>console.log(`Connected @${PORT}`))
+app.listen(port,()=>console.log(`Connected @${PORT}`))
